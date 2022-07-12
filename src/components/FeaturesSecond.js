b5b27aea71bab866fec045ec4ccda3b9f1d635a5
Tio Dwi Satrio <tiodwisatrio27@gmail.com>
2022-07-13 05:11:12 +0700

    first_commit

--- src/components/FeaturesSecond.js
+++ src/components/FeaturesSecond.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
+// import data
+import {features } from '../data'
+
+
 const FeaturesSecond = () => {
-  return <div>FeaturesSecond</div>;
+  const {title, subtitle, image } = features.feature2;
+  return (
+    <section className='section'>
+    <div className="container mx-auto">
+      <div className='flex flex-col lg:flex-row lg: gap-[100px] items-center'>
+
+         {/* text */}
+         <div className='flex-1 flex-col justify-end '>
+          <h2 className='title'>{title}</h2>
+          <p className='subtitle'>{subtitle}</p>
+        </div>
+
+        {/* image */}
+        <div className='flex-1 '>
+          <img src={image.type} alt="image_type" />
+        </div>
+
+       
+      </div>
+    </div>
+  </section>
+  )
+  
 };
 
 export default FeaturesSecond;
